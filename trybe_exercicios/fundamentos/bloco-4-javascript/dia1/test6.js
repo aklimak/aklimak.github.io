dafcf0e2804bca06fba9f5bdb5db4d45c3539cf8
aklimak <alexandre.skk@gmail.com>
2021-08-08 09:42:58 -0300

    Adiciona exercicio 6

---
+++ trybe_exercicios/fundamentos/bloco-4-javascript/dia1/test6.js
@@ -0,0 +1,11 @@
+let peca = "bishop";
+
+if (peca === "horse") {
+    console.log("Moves in L shape, 3x2");
+}
+else if (peca === "bishop") {
+    console.log("Moves only in diagonals");
+}
+else if (peca === "tower") {
+    console.log("Moves only in horizontal and vertical directions");
+}
