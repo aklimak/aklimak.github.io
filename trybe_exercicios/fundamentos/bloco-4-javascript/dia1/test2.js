10e6045835c6ae4dc934e6ccc424831909e5df22
aklimak <alexandre.skk@gmail.com>
2021-08-08 09:08:26 -0300

    Adiciona exercicio 3

---
+++ trybe_exercicios/fundamentos/bloco-4-javascript/dia1/test2.js
@@ -0,0 +1,13 @@
+const a = 2;
+const b = 5;
+const c = 9;
+
+if (a > b && a > c) {
+    console.log(a);
+}
+else if (b > a && b > c) {
+    console.log(b);
+}
+else if (c > a && c > b) {
+    console.log(c);
+}
